07a3ae888559552dd78aa92314e4c71fd2686953
imdalai <34291560+imdalai@users.noreply.github.com>
2017-12-06 20:27:05 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # imdalai.github.io
+
+This is a tool to edit Mongolian word online , [https://imdalai.github.io/](https://imdalai.github.io/)
---
+++ js/common.js
@@ -0,0 +1,8 @@
+/**
+ * @author imdalai
+ * @date 2017-12-06
+ * @description set title
+ */
+$(document).ready(function () {
+    $("title").html("\u1832\u1820\u182f\u1820\u1822");
+});
